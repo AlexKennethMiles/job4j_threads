3bde7bda525ca36f72fcafd8bdc8b55f41b3053a
AlexKennethMiles <apuchkov17@gmail.com>
2022-08-12 20:07:40 +0300

    1. Синхронизация общих ресурсов. [#1096]

--- src/main/java/ru/job4j/Cache.java
+++ src/main/java/ru/job4j/Cache.java
@@ -3,7 +3,13 @@ package ru.job4j;
 public final class Cache {
     private static Cache cache;
 
-    public static Cache instOf() {
+    /**
+     * Метод модифицирован синхронизацией, что ограничивает возможность обращения к
+     * этому методу одного экземпляра класса двумя (или более) разными нитями.
+     * Таким образом, выполняется условие атомарности.
+     * @return - возвращает экземпляр класса Cache
+     */
+    public synchronized static Cache instOf() {
         if (cache == null) {
             cache = new Cache();
         }
