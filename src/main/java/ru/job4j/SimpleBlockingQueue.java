0759dcf088444befc7e105afa05def9efd336737
AlexKennethMiles <apuchkov17@gmail.com>
2022-08-16 21:07:04 +0300

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ src/main/java/ru/job4j/SimpleBlockingQueue.java
@@ -0,0 +1,48 @@
+package ru.job4j;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+    private int limit;
+
+    public SimpleBlockingQueue(final int limit) {
+        this.limit = limit;
+    }
+
+    public synchronized void offer(T value) {
+        while (queue.size() == limit) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        if (queue.size() < limit) {
+            notifyAll();
+        }
+        queue.offer(value);
+        limit++;
+    }
+
+    public synchronized T poll() {
+        while (queue.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        if (queue.size() == limit) {
+            notifyAll();
+        }
+        limit--;
+        return queue.poll();
+    }
+}
---
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -0,0 +1,87 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SimpleBlockingQueueTest {
+    @Test
+    public void whenProducerFillsTheQueue() throws InterruptedException {
+        SimpleBlockingQueue<Integer> sbq = new SimpleBlockingQueue<>(5);
+        Queue<Integer> rsl = new LinkedList<>();
+        int count = 10;
+        Thread producer = new Thread(
+                () -> {
+                    for (int i = 0; i < count; i++) {
+                        sbq.offer(i);
+                    }
+                }
+        );
+        Thread consumer = new Thread(
+                () -> {
+                    for (int i = count; i > 0; i--) {
+                        rsl.add(sbq.poll());
+                    }
+                }
+        );
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+        assertThat(rsl).isEqualTo(new LinkedList<>(List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)));
+    }
+
+    @Test
+    public void whenConsumerTriesToTakeElButEmptyQueue() throws InterruptedException {
+        SimpleBlockingQueue<Integer> sbq = new SimpleBlockingQueue<>(5);
+        Queue<Integer> rsl = new LinkedList<>();
+        int count = 10;
+        Thread producer = new Thread(
+                () -> System.out.println("No offer")
+        );
+        Thread consumer = new Thread(
+                () -> {
+                    for (int i = count; i > 0; i--) {
+                        rsl.add(sbq.poll());
+                    }
+                }
+        );
+        producer.start();
+        consumer.start();
+        producer.join();
+        assertThat(rsl).isEqualTo(new LinkedList<Integer>(List.of()));
+    }
+
+    @Test
+    public void whenProducerFillsTheQueueButFullQueue() throws InterruptedException {
+        SimpleBlockingQueue<Integer> sbq = new SimpleBlockingQueue<>(5);
+        Queue<Integer> rsl = new LinkedList<>();
+        int count = 5;
+        for (int i = 0; i < count; i++) {
+            sbq.offer(i);
+        }
+        Thread producer = new Thread(
+                () -> {
+                    for (int i = 0; i < count; i++) {
+                        sbq.offer(i);
+                    }
+                }
+        );
+        Thread consumer = new Thread(
+                () -> {
+                    for (int i = count; i > 0; i--) {
+                        rsl.add(sbq.poll());
+                    }
+                }
+        );
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+        assertThat(rsl).isEqualTo(new LinkedList<>(List.of(0, 1, 2, 3, 4)));
+    }
+}
