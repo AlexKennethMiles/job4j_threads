8d5a1e1256dd93570b6157586f7266840b629db0
AlexKennethMiles <apuchkov17@gmail.com>
2023-03-05 18:40:06 +0300

    3. Immutable объекты [#267918 #332274]

---
+++ src/main/java/ru/job4j/linked/Node.java
@@ -0,0 +1,24 @@
+package ru.job4j.linked;
+
+/**
+ * Создали immutable класс: все поля private final, нет сеттеров,
+ * и класс не может быть унаследован (модификатор final class).
+ * Если бы были поля с ссылочными объектами, их надо было бы возвращать через копии.
+ */
+public final class Node<T> {
+    private final Node<T> next;
+    private final T value;
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
